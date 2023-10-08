fb50c3837bc71c1fdaf7138e47628ac408ae94f8
jimczjchen <jimczjchen@tencent.com>
2023-10-08 21:57:44 +0800

    feat: intent 协议支持

--- android/src/main/java/com/github/jzoom/linker/LinkerPlugin.java
+++ android/src/main/java/com/github/jzoom/linker/LinkerPlugin.java
@@ -37,7 +37,7 @@ public class LinkerPlugin implements MethodCallHandler, PluginRegistry.ActivityR
   /** Plugin registration. */
   public static void registerWith(Registrar registrar) {
     final MethodChannel channel = new MethodChannel(registrar.messenger(), "linker");
-    LinkerPlugin linkerPlugin = new LinkerPlugin(channel,registrar.activity());
+    LinkerPlugin linkerPlugin = new LinkerPlugin(channel, registrar.activity());
     registrar.addActivityResultListener(linkerPlugin);
     channel.setMethodCallHandler(linkerPlugin);
   }
@@ -46,49 +46,49 @@ public class LinkerPlugin implements MethodCallHandler, PluginRegistry.ActivityR
 
   private int requestCode;
 
-
-  static void setValueToIntent(String name, Object value, Intent map){
-    if(value instanceof String) {
-      map.putExtra(name, (String)value);
-    } else if(value instanceof Integer) {
-      map.putExtra(name, ((Integer)value).intValue());
-    } else if(value instanceof Float) {
-      map.putExtra(name, ((Float)value).floatValue());
-    } else if(value instanceof Double) {
-      map.putExtra(name, ((Double)value).doubleValue());
-    } else if(value instanceof Boolean) {
-      map.putExtra(name, ((Boolean)value).booleanValue());
-    }else{
+  static void setValueToIntent(String name, Object value, Intent map) {
+    if (value instanceof String) {
+      map.putExtra(name, (String) value);
+    } else if (value instanceof Integer) {
+      map.putExtra(name, ((Integer) value).intValue());
+    } else if (value instanceof Float) {
+      map.putExtra(name, ((Float) value).floatValue());
+    } else if (value instanceof Double) {
+      map.putExtra(name, ((Double) value).doubleValue());
+    } else if (value instanceof Boolean) {
+      map.putExtra(name, ((Boolean) value).booleanValue());
+    } else {
       throw new RuntimeException("Unsupported type:" + value.getClass());
     }
   }
 
-  Map<String,Object> bundle2Map(Bundle bundle){
-    if(bundle==null){
+  Map<String, Object> bundle2Map(Bundle bundle) {
+    if (bundle == null) {
       return null;
     }
-    Map<String,Object> data = new HashMap<>();
+    Map<String, Object> data = new HashMap<>();
 
     Set<String> set = bundle.keySet();
-    for (String key:set) {
+    for (String key : set) {
       /// Notice the value must be double/int/boolean/String
-      data.put(key,bundle.get(key));
+      data.put(key, bundle.get(key));
     }
 
     return data;
 
   }
+
   private Map intent2map(Intent intent) {
     Map map = new HashMap();
-    map.put("action",intent);
-    map.put("extras",intent.getExtras());
-
+    map.put("action", intent);
+    map.put("extras", intent.getExtras());
 
     return map;
 
   }
-  private Intent parseIntent( Map data ){
-    Map<String,Object> extras = (Map<String,Object>) data.get("extras");
+
+  private Intent parseIntent(Map data) {
+    Map<String, Object> extras = (Map<String, Object>) data.get("extras");
     Intent intent = null;
     String uri = (String) data.get("uri");
     String action = (String) data.get("action");
@@ -102,32 +102,41 @@ public class LinkerPlugin implements MethodCallHandler, PluginRegistry.ActivityR
       intent.putExtra("LauncherUI.Shortcut.LaunchType", "launch_type_offline_wallet");
       return intent;
     }
-    if(action!=null){
-      if(uri!=null){
+    // intent 协议处理
+    if (uri != null && uri.startsWith("intent:") && uri.contains("#Intent")) {
+      try {
+        intent = Intent.parseUri(uri, Intent.URI_INTENT_SCHEME);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_DOCUMENT);
+        return intent;
+      } catch (Exception e) {
+      }
+    }
+    if (action != null) {
+      if (uri != null) {
         intent = new Intent(action, Uri.parse(uri));
-      }else{
+      } else {
         intent = new Intent(action);
       }
     }
 
-    if(intent==null){
+    if (intent == null) {
       intent = new Intent();
     }
 
     String packageName = (String) data.get("packageName");
     String className = (String) data.get("className");
-    if(packageName!=null && className!=null){
+    if (packageName != null && className != null) {
       intent.setClassName(packageName, className);
     }
 
-    if(extras!=null){
-      for(Map.Entry<String,Object> entry : extras.entrySet()){
+    if (extras != null) {
+      for (Map.Entry<String, Object> entry : extras.entrySet()) {
         String key = entry.getKey();
         Object value = entry.getValue();
-        if(value==null){
+        if (value == null) {
           continue;
         }
-        setValueToIntent(key,value,intent);
+        setValueToIntent(key, value, intent);
       }
     }
     intent.addFlags(Intent.FLAG_ACTIVITY_NEW_DOCUMENT);
@@ -139,34 +148,34 @@ public class LinkerPlugin implements MethodCallHandler, PluginRegistry.ActivityR
   public void onMethodCall(MethodCall call, Result result) {
     String method = call.method;
     if ("startActivityForResult".equals(method)) {
-      synchronized (this){
-        if(this.result!=null){
-          this.result.error("cancel","The last result is not finished","");
+      synchronized (this) {
+        if (this.result != null) {
+          this.result.error("cancel", "The last result is not finished", "");
           this.result = null;
         }
 
         Map data = (Map) call.arguments;
         this.result = result;
-        this.requestCode = (Integer)data.get("requestCode");
+        this.requestCode = (Integer) data.get("requestCode");
         Intent intent = parseIntent(data);
-        try{
-          activity.startActivityForResult(intent,requestCode);
-        }catch (Throwable e){
-          result.error(e.getClass().getName(),e.getMessage(),"Cannot start activity");
+        try {
+          activity.startActivityForResult(intent, requestCode);
+        } catch (Throwable e) {
+          result.error(e.getClass().getName(), e.getMessage(), "Cannot start activity");
           this.result = null;
         }
       }
-    }else if("startActivity".equals(method)){
+    } else if ("startActivity".equals(method)) {
       Map data = (Map) call.arguments;
       Intent intent = parseIntent(data);
-      try{
+      try {
         activity.startActivity(intent);
         result.success(true);
-      }catch (Throwable e){
-        result.error(e.getClass().getName(),e.getMessage(),"Cannot start activity");
+      } catch (Throwable e) {
+        result.error(e.getClass().getName(), e.getMessage(), "Cannot start activity");
       }
 
-    }else if("openSetting".equals(method)){
+    } else if ("openSetting".equals(method)) {
       Intent intent = new Intent();
       intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
       if (Build.VERSION.SDK_INT >= 9) {
@@ -174,13 +183,13 @@ public class LinkerPlugin implements MethodCallHandler, PluginRegistry.ActivityR
         intent.setData(Uri.fromParts("package", getPackageName(), null));
       } else if (Build.VERSION.SDK_INT <= 8) {
         intent.setAction(Intent.ACTION_VIEW);
-        intent.setClassName("com.android.settings","com.android.settings.InstalledAppDetails");
+        intent.setClassName("com.android.settings", "com.android.settings.InstalledAppDetails");
         intent.putExtra("com.android.settings.ApplicationPkgName", getPackageName());
       }
       activity.startActivity(intent);
       result.success(true);
     } else if ("openBackgroundSetting".equals(method)) {
-      Intent intent =  new Intent(Settings.ACTION_SETTINGS);    
+      Intent intent = new Intent(Settings.ACTION_SETTINGS);
       activity.startActivity(intent);
       result.success(true);
       // result.success(goSettingPage());
@@ -222,7 +231,7 @@ public class LinkerPlugin implements MethodCallHandler, PluginRegistry.ActivityR
       if (isSmartisan()) {
         goSmartisanSetting();
         return true;
-      }      
+      }
     } catch (Exception e) {
     }
     return false;
@@ -241,10 +250,10 @@ public class LinkerPlugin implements MethodCallHandler, PluginRegistry.ActivityR
 
   private void goHonorSetting() {
     try {
-      
+
       showActivity("com.hihonor.devicemanager");
     } catch (Exception e) {
-      Log.e("jimtest",e.getMessage());
+      Log.e("jimtest", e.getMessage());
     }
   }
 
@@ -255,17 +264,17 @@ public class LinkerPlugin implements MethodCallHandler, PluginRegistry.ActivityR
 
   private void goOPPOSetting() {
     try {
-        showActivity("com.coloros.phonemanager");
+      showActivity("com.coloros.phonemanager");
     } catch (Exception e1) {
+      try {
+        showActivity("com.oppo.safe");
+      } catch (Exception e2) {
         try {
-            showActivity("com.oppo.safe");
-        } catch (Exception e2) {
-            try {
-                showActivity("com.coloros.oppoguardelf");
-            } catch (Exception e3) {
-                showActivity("com.coloros.safecenter");
-            }
+          showActivity("com.coloros.oppoguardelf");
+        } catch (Exception e3) {
+          showActivity("com.coloros.safecenter");
         }
+      }
     }
   }
 
@@ -282,9 +291,9 @@ public class LinkerPlugin implements MethodCallHandler, PluginRegistry.ActivityR
 
   private void goSamsungSetting() {
     try {
-        showActivity("com.samsung.android.sm_cn");
+      showActivity("com.samsung.android.sm_cn");
     } catch (Exception e) {
-        showActivity("com.samsung.android.sm");
+      showActivity("com.samsung.android.sm");
     }
   }
 
@@ -301,18 +310,18 @@ public class LinkerPlugin implements MethodCallHandler, PluginRegistry.ActivityR
    * 跳转到指定应用的首页
    */
   private void showActivity(String packageName) {
-      Intent intent = activity.getPackageManager().getLaunchIntentForPackage(packageName);
-      activity.startActivity(intent);
+    Intent intent = activity.getPackageManager().getLaunchIntentForPackage(packageName);
+    activity.startActivity(intent);
   }
 
   /**
    * 跳转到指定应用的指定页面
    */
   private void showActivity(String packageName, String activityDir) {
-      Intent intent = new Intent();
-      intent.setComponent(new ComponentName(packageName, activityDir));
-      intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-      activity.startActivity(intent);
+    Intent intent = new Intent();
+    intent.setComponent(new ComponentName(packageName, activityDir));
+    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+    activity.startActivity(intent);
   }
 
   // 是否是华为
@@ -354,24 +363,20 @@ public class LinkerPlugin implements MethodCallHandler, PluginRegistry.ActivityR
     return Build.BRAND != null && Build.BRAND.toLowerCase().equals("smartisan");
   }
 
-
-
   private String getPackageName() {
     return activity.getPackageName();
   }
 
-
   @Override
   public boolean onActivityResult(int requestCode, int resultCode, Intent intent) {
-    synchronized (this){
-      if(requestCode == this.requestCode && result!=null){
-        Map<String,Object> data = new HashMap<>();
-        data.put("resultCode",resultCode);
-        data.put("requestCode",requestCode);
-        if(intent!=null){
-          data.put("intent", intent2map(intent) );
+    synchronized (this) {
+      if (requestCode == this.requestCode && result != null) {
+        Map<String, Object> data = new HashMap<>();
+        data.put("resultCode", resultCode);
+        data.put("requestCode", requestCode);
+        if (intent != null) {
+          data.put("intent", intent2map(intent));
         }
-
 
         result.success(data);
         this.result = null;
@@ -380,6 +385,5 @@ public class LinkerPlugin implements MethodCallHandler, PluginRegistry.ActivityR
     }
     return false;
   }
-
 
 }
